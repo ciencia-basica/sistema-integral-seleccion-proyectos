1b1b31936e5c917e3e7ee3f047fa7542eeeee836
YoAquinjs <sebascertuche@gmail.com>
2024-11-18 17:10:04 -0600

    debug and refactor run-server to tmux

--- backend/deploy/run-server.sh
+++ backend/deploy/run-server.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
-# Dependencies: screen
+# Dependencies: tmux
 # run after running successfully setup-server.sh
 
 # expected environment variables
 # SISP_PYVENV="python virtualenv dirname"
-# SISP_SCREEN_NAME="name of the screen session running the server"
+# SISP_SESSION_NAME="name of the tmux session running the server"
 
-cd "$(dirname "${BASH_SOURCE[0]}")../" || exit 1
+cd "$(dirname "${BASH_SOURCE[0]}")/../" || exit 1
 
-screen -S "$SISP_SCREEN_NAME" -dm bash -c "
-    set -e
-    virtualenv \"$SISP_PYVENV\"
-    python3 src/main.py
+if tmux has-session -t "$SISP_SESSION_NAME" &>/dev/null; then
+    echo "Tmux session '$SISP_SESSION_NAME' already exists. Attaching..."
+    tmux attach-session -t "$SISP_SESSION_NAME"
+    exit 0
+fi
+
+tmux new-session -d -s "$SISP_SESSION_NAME" bash -c "
+    source \"$SISP_PYVENV/bin/activate\";
+    python3 src/main.py;
 "
 
-screen -r "$SISP_SCREEN_NAME"
+tmux attach-session -t "$SISP_SESSION_NAME"
