03e148a8682df53b6785ddd079df3c6db1c5a594
YoAquinjs <sebascertuche@gmail.com>
2024-11-17 19:57:09 -0600

    add java to dependencies

--- backend/install-dependencies.sh
+++ backend/install-dependencies.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # snap and git are assumed to be installed
-# installs python, certbot, nginx
+# installs python, certbot, nginx, java
 
-sudo apt-get update && sudo apt-get upgrade -y
+sudo apt update && sudo apt upgrade -y
 
 if ! python3 &> /dev/null || ! pip3 &> /dev/null ; then
     echo "installing python..."
-    sudo apt-get install python3
-    sudo apt-get install python3-pip
+    sudo apt install python3
+    sudo apt install python3-pip
 fi
 
 if ! nginx &> /dev/null; then
     echo "installing nginx..."
-    sudo apt-get install nginx
+    sudo apt install nginx
     sudo systemctl enable nginx
 fi
 
@@ -21,4 +21,9 @@ if ! certbot &> /dev/null; then
     echo "installing certbot..."
     sudo snap install --classic certbot
     sudo ln -s /snap/bin/certbot /usr/bin/certbot
+fi
+
+if ! java &> /dev/null; then
+    echo "installing java..."
+    sudo apt install openjdk-21-jre-headless
 fi
